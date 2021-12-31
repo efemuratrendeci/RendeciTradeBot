378acab9428eb8adc5b5438382c8bca8413595ed
Efe Rendeci <efemuratrendeci@hotmail.com>
2021-12-31 03:23:11 +0300

    sell limit fix

--- models/CoinObservation.js
+++ models/CoinObservation.js
@@ -22,7 +22,7 @@ class CoinObservation {
         return this.observation_route 
             && this.observation_route[this.observation_route.length - 1] === '-' 
             && this.bought_price > this.current_observation[this.current_observation.length - 1] 
-            && this.current_observation[this.current_observation.length - 1] / this.bought_price <= 0.995 ? true : false;
+            && this.current_observation[this.current_observation.length - 1] / this.bought_price <= 0.99 ? true : false;
     }
 
     static get is_price_over_one_percent() {
